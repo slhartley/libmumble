c43bf102f6e243d8ea040fe7033f32d3d89ca562
slhartley <36980379+slhartley@users.noreply.github.com>
2026-09-13 07:09:44 -0500

    Refactor wincompile.bat for clarity and efficiency
    Removed redundant commands and added comments for clarity.

--- wincompile.bat
+++ wincompile.bat
@@ -1,5 +1,6 @@
 rmdir /s /q "build"
 cmake -S . -B build -A x64 -G "Visual Studio 17 2022"
 cmake --build build --config  Debug
+REM copy libraries to the executables location
 copy build\debug\*.*	build\example\Debug
-copy altproject\examples\ExampleServer\*.pem build\example\Debug
+
