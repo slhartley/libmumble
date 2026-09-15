0a69ca67c826279a345ccaab2716b1402e9a5026
slhartley <36980379+slhartley@users.noreply.github.com>
2026-09-15 08:21:44 -0500

    Refactor wincompile.bat for clarity and efficiency
    Removed redundant commands and added a comment for copying PEM files.

--- wincompile.bat
+++ wincompile.bat
@@ -3,4 +3,5 @@ cmake -S . -B build -A x64 -G "Visual Studio 17 2022"
 cmake --build build --config  Debug
 REM copy libraries to the executables location
 copy build\debug\*.*	build\example\Debug
+REM copy altproject\examples\ExampleServer\*.pem build\example\Debug
 
